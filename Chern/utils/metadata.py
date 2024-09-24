4bde1747ef144aba0a2c01967f3e930a57137587
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2024-09-24 22:07:06 +0800

    Refine the style

--- Chern/utils/metadata.py
+++ Chern/utils/metadata.py
@@ -1,112 +1,121 @@
 import json
 import os
 import yaml
+import fcntl  # For Unix-based systems
+
 
 class ConfigFile(object):
-    """ ConfigFile class, it is used to read and write the metadata.
-    In the version 3 of ``Chern'', metadata is saved in a json file rather than a python file.
-    It will support three types:
-        dict,
-        list,
-          and
-        string
+    """ConfigFile class used to read and write metadata in a JSON file.
+   
+    It supports three types:
+        - dict
+        - list
+        - string
     """
 
     def __init__(self, file_path):
-        """ Initialize the class use a path
-        Create a file if it is not initially exists
+        """Initialize the class with a file path.
+        
+        Create the file if it does not initially exist.
         """
         self.file_path = file_path
 
     def read_variable(self, variable_name, default=None):
-        """ Get the content of some variable
+        """Get the content of a variable from the JSON file.
+        
+        Args:
+            variable_name (str): The name of the variable to read.
+            default: The default value to return if the variable is not found.
+        
+        Returns:
+            The value of the variable or the default value.
         """
-        file_path = self.file_path
-        if not os.path.exists(file_path):
+        if not os.path.exists(self.file_path):
             return default
-        with open(file_path, encoding='utf-8') as f:
+        with open(self.file_path, encoding='utf-8') as f:
             contents = f.read()
-            if contents == "" or contents.isspace():
+            if not contents.strip():
                 return default
-            d = json.loads(contents)
-            return d.get(variable_name, default)
+            data = json.loads(contents)
+            return data.get(variable_name, default)
 
     def write_variable(self, variable_name, value):
-        file_path = self.file_path
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        if not os.path.exists(file_path):
-            with open(file_path, "w") as f:
+        """Write a variable to the JSON file.
+        
+        Args:
+            variable_name (str): The name of the variable to write.
+            value: The value to write.
+        """
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, "w") as f:
                 json.dump({}, f)
-        try_times = 10 ** 4
-        for i in range(try_times):
-            if not os.path.exists(file_path + ".lock"):
-                break
-        if os.path.exists(file_path + ".lock"):
-            raise os.error
 
-        open(file_path+".lock", "a").close()
-
-        with open(file_path, encoding='utf-8') as f:
+        with open(self.file_path, "r+", encoding='utf-8') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             contents = f.read()
-            if contents == "" or contents.isspace():
-                d = {}
-            else:
-                d = json.loads(contents)
+            data = json.loads(contents) if contents.strip() else {}
+            data[variable_name] = value
+            f.seek(0)
+            f.truncate()
+            json.dump(data, f)
+            fcntl.flock(f, fcntl.LOCK_UN)
 
-        d[variable_name] = value
-        with open(file_path, "w") as f:
-            json.dump(d, f)
-
-        os.remove(file_path+".lock")
 
 class YamlFile(object):
-    """ ConfigFile class, it is used to read and write the metadata.
-    yaml file is similar to json, but it is somewhat more-human readable
+    """YamlFile class used to read and write metadata in a YAML file.
+    
+    YAML files are similar to JSON but are more human-readable.
     """
 
     def __init__(self, file_path):
-        """ Initialize the class use a path
-        Create a file if it is not initially exists
+        """Initialize the class with a file path.
+        
+        Create the file if it does not initially exist.
         """
         self.file_path = file_path
 
     def read_variable(self, variable_name, default=None):
-        """ Get the content of some variable
+        """Get the content of a variable from the YAML file.
+        
+        Args:
+            variable_name (str): The name of the variable to read.
+            default: The default value to return if the variable is not found.
+
+        Returns:
+            The value of the variable or the default value.
         """
-        file_path = self.file_path
-        if not os.path.exists(file_path):
+        if not os.path.exists(self.file_path):
             return default
-        with open(file_path, encoding='utf-8') as f:
+        with open(self.file_path, encoding='utf-8') as f:
             contents = f.read()
-            if contents == "" or contents.isspace():
+            if not contents.strip():
                 return default
-            d = yaml.load(contents, Loader=yaml.Loader)
-            return d.get(variable_name, default)
+            data = yaml.load(contents, Loader=yaml.Loader)
+            return data.get(variable_name, default)
 
     def write_variable(self, variable_name, value):
-        file_path = self.file_path
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        if not os.path.exists(file_path):
-            with open(file_path, "w") as f:
-                json.dump({}, f)
-        try_times = 10 ** 4
-        for i in range(try_times):
-            if not os.path.exists(file_path + ".lock"):
-                break
-        if os.path.exists(file_path + ".lock"):
-            raise os.error
+        """Write a variable to the YAML file.
 
-        open(file_path+".lock", "a").close()
+        Args:
+            variable_name (str): The name of the variable to write.
+            value: The value to write.
+        """
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, "w") as f:
+                yaml.dump({}, f)
 
-        with open(file_path, encoding='utf-8') as f:
+        with open(self.file_path, "r+", encoding='utf-8') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             contents = f.read()
-            if contents == "" or contents.isspace():
-                d = {}
-            else:
-                d = yaml.load(contents, Loader=yaml.Loader)
-
-        d[variable_name] = value
-        with open(file_path, "w") as f:
-            yaml.dump(d, f)
-
-        os.remove(file_path+".lock")
+            data = (
+                yaml.load(contents, Loader=yaml.Loader)
+                if contents.strip()
+                else {}
+            )
+            data[variable_name] = value
+            f.seek(0)
+            f.truncate()
+            yaml.dump(data, f)
+            fcntl.flock(f, fcntl.LOCK_UN)
