e4c838e629f44382a83495e132317598546138e3
rabbit <rabbit@hole>
2025-03-17 15:06:58 +0800

    Fix linter problem

--- Chern/kernel/valgorithm.py
+++ Chern/kernel/valgorithm.py
@@ -3,15 +3,17 @@
 import os
 import subprocess
 
-from .vobject import VObject
-from .chern_cache import ChernCache
-
 from ..utils import csys
 from ..utils.pretty import colorize
 from ..utils import metadata
 
+from .chern_cache import ChernCache
 from .chern_communicator import ChernCommunicator
-cherncache = ChernCache.instance()
+from . import helpme
+from .vobject import VObject
+from .vobj_file import LsParameters
+
+CHERN_CACHE = ChernCache.instance()
 
 
 class VAlgorithm(VObject):
@@ -19,23 +21,24 @@ class VAlgorithm(VObject):
     """
 
     def helpme(self, command):
-        from Chern.kernel.Helpme import algorithm_helpme
-        print(algorithm_helpme.get(command, "No such command, try ``helpme'' alone."))
+        """ Helpme function """
+        print(helpme.algorithm_helpme.get(command, "No such command, try ``helpme'' alone."))
 
 
     def print_status(self):
-        print("Status of task: {}".format(self.invariant_path()))
+        """ Print the status """
+        print(f"Status of task: {self.invariant_path()}")
         if self.status() == "impressed":
-            print("Impression: [{}]".format(colorize(self.impression().uuid, "success")))
+            print(f"Impression: [{colorize(self.impression().uuid, 'success')}]")
         else:
-            print("Impression: [{}]".format(colorize("New", "warning")))
+            print(f"Impression: [{colorize('New','warning')}]")
             return
         cherncc = ChernCommunicator.instance()
         dite_status = cherncc.dite_status()
         if dite_status == "ok":
-            print("DIET: [{}]".format(colorize("connected", "success")))
+            print(f"DIET: [{colorize('connected','success')}]")
         else:
-            print("DIET: [{}]".format(colorize("unconnected", "warning")))
+            print(f"DIET: [{colorize('unconnected','warning')}]")
             return
 
         deposited = cherncc.is_deposited(self.impression())
@@ -54,21 +57,21 @@ class VAlgorithm(VObject):
             print(colorize("**** WORKFLOW:", "title0"))
             runner = workflow_check[0]
             workflow = workflow_check[1]
-            print("Workflow: [{}][{}]".format(colorize(runner,"success"), colorize(workflow, "success")))
+            print(f"Workflow: [{colorize(runner,'success')}][{colorize(workflow,'success')}]")
 
             files = cherncc.output_files(self.impression(), runner)
             print("Output files (collected on DIET):")
             for f in files:
-                print("    {}".format(f))
+                print(f"    {f}")
 
 
-    def status(self, consult_id = None, detailed = False):
+    def status(self, consult_id = None):
         """ Consult the status of the object
             There should be only two status locally: new|impressed
         """
         # If it is already asked, just give us the answer
         if consult_id:
-            consult_table = cherncache.status_consult_table
+            consult_table = CHERN_CACHE.status_consult_table
             cid, status = consult_table.get(self.path, (-1,-1))
             if cid == consult_id:
                 return status
@@ -83,15 +86,19 @@ class VAlgorithm(VObject):
             consult_table[self.path] = (consult_id, status)
         return status
 
+    def job_status(self):
+        """ Asking for the remote status
+        """
+        cherncc = ChernCommunicator.instance()
+        return cherncc.job_status(self.impression())
 
-
-    def run_status(self, consult_id = None):
+    def run_status(self):
         """ Asking for the remote status
         """
         cherncc = ChernCommunicator.instance()
         return cherncc.status(self.impression())
 
-    def is_submitted(self):
+    def is_submitted(self, runner="local"):
         """ Judge whether submitted or not. Return a True or False.
         [FIXME: incomplete]
         """
@@ -99,29 +106,19 @@ class VAlgorithm(VObject):
             return False
         return False
 
-    def resubmit(self):
-        pass
+    def resubmit(self, runner="local"):
+        """ Resubmit """
         # FIXME: fixit later
 
-    def stdout(self):
-        """ stdout
-        """
-        with open(self.image().path+"/stdout") as stdout_file:
-            return stdout_file.read()
 
-    def stderr(self):
-        """ Std error
-        """
-        with open(self.image().path+"/stderr") as stderr_file:
-            return stderr_file.read()
-
-    def ls(self, show_readme=True, show_predecessors=True, show_sub_objects=True, show_status=True, show_successors=False):
+    def ls(self, show_info=LsParameters()):
         """ list the infomation.
         """
-        super(VAlgorithm, self).ls(show_readme, show_predecessors, show_sub_objects, show_status, show_successors)
+        super().ls(show_info)
 
-        if show_status:
+        if show_info.status:
             status = self.status()
+            status_color = ""
             if status == "new":
                 status_color = "normal"
             elif status == "impressed":
@@ -132,17 +129,15 @@ class VAlgorithm(VObject):
             if status == "impressed":
                 run_status = self.run_status()
                 if run_status != "unconnected":
-                    if (run_status == "unsubmitted"):
+                    if run_status == "unsubmitted":
                         status_color = "warning"
-                    elif (run_status == "failed"):
+                    elif run_status == "failed":
                         status_color = "warning"
                     else:
                         status_color = "success"
                         status_str += colorize("["+run_status+"]", status_color)
             print(colorize("**** STATUS:", "title0"), status_str)
 
-        if self.is_submitted() and self.image().error() != "":
-            print(colorize("!!!! ERROR:\n", "title0"), self.image().error())
 
         print(colorize("---- Files:", "title0"))
         files = os.listdir(self.path)
@@ -151,6 +146,7 @@ class VAlgorithm(VObject):
                 print(f)
 
     def commands(self):
+        """ Get the commands from the yaml file """
         yaml_file = metadata.YamlFile(os.path.join(self.path, "chern.yaml"))
         return yaml_file.read_variable("commands", [])
 
@@ -178,11 +174,13 @@ class VAlgorithm(VObject):
         """
         obj = VObject(path)
         if obj.object_type() != "algorithm":
-            print("You are adding {} type object as input. The input is required to be an algorithm.".format(obj.object_type()))
+            print(f"You are adding {obj.object_type()} type object as input."
+                  " The input is required to be an algorithm.")
             return
 
         if obj.has_predecessor_recursively(self):
-            print("The object is already in the dependency diagram of the ``input'', which will cause a loop.")
+            print("The object is already in the dependency diagram of"
+                  " the ``input'', which will cause a loop.")
             return
 
         if self.has_alias(alias):
@@ -196,37 +194,30 @@ class VAlgorithm(VObject):
         self.set_alias(alias, obj.invariant_path())
 
     def remove_input(self, alias):
+        """ Remove the input """
         path = self.alias_to_path(alias)
         if path == "":
             print("Alias not found")
             return
         project_path = csys.project_path(self.path)
         obj = VObject(project_path+"/"+path)
-        self.remove_arc_from()
+        self.remove_arc_from(obj)
         self.remove_alias(alias)
 
-    def importfile(self, filename):
-        """
-        Import the file to this task directory
-        """
-        csys.copy(filename, self.path)
-
-
-
-
-
-
 def create_algorithm(path, use_template=False):
+    """ Create an algorithm """
     path = csys.strip_path_string(path)
     os.mkdir(path)
-    os.mkdir(path+"/.chern")
-    config_file = metadata.ConfigFile(path+"/.chern/config.json")
+    os.mkdir(f"{path}/.chern")
+    config_file = metadata.ConfigFile(f"{path}/.chern/config.json")
     config_file.write_variable("object_type", "algorithm")
 
-    with open(path + "/.chern/README.md", "w") as readme_file:
+    with open(f"{path}/.chern/README.md", "w", encoding="utf-8") as readme_file:
         readme_file.write("Please write README for this algorithm")
-    subprocess.call("vim {}/.chern/README.md".format(path), shell=True)
+    subprocess.call(f"vim {path}/.chern/README.md", shell=True)
     if use_template:
         template_name = input("Please input the Dockerfile template type")
-        print("Creating template, but hahahaha")
-        return
+        print("Creating template, but ...")
+        print("Not implemented yet.")
+        print(f"Template name: {template_name}")
+        # FIXME: implement it later
